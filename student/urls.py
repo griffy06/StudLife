1e2856735384fe524c6bcfdc8ce958d1e4f0d34f
griffy06 <hi2cheena@gmail.com>
2020-03-19 17:01:10 +0530

    added functions of student

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('', views.UserFormView.as_view(), name="student_login"),
+    path('<int:user_id>/',views.logged_in, name="logged_in"),
+    path('<int:user_id>/outpass/',views.outpass, name="outpass"),
+    path('<int:user_id>/order_food/',views.order_food, name="order_food"),
+    path('<int:user_id>/book_appointment/',views.book_appointment, name="book_appointment"),
+    path('<int:user_id>/digilocker/',views.digilocker, name="digilocker"),
 ]
--- student/views.py
+++ student/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 
 
 class UserFormView(View):
@@ -9,3 +13,45 @@ class UserFormView(View):
     def get(self, request):
         return render(request, self.template_name)
 
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('logged_in', user.id)
+        else:
+            return render(request, self.template_name)
+
+
+@login_required()
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def logged_in(request,user_id):
+
+    user = User.objects.get(pk=user_id)
+    return render(request,'student/student_page.html',{'user':user})
+
+
+@login_required()
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def outpass(request,user_id):
+
+    return render(request,'student/outpass_page.html')
+
+
+@login_required()
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def book_appointment(request, user_id):
+    return render(request, 'student/book_appointment_page.html')
+
+
+@login_required()
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def order_food(request, user_id):
+    return render(request, 'student/order_food_page.html')
+
+
+@login_required()
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def digilocker(request, user_id):
+    return render(request, 'student/digilocker_page.html')
