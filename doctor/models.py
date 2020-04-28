585074d955499e2392ce5711bb9785e9a63920b4
Rohini Mishra <iit2018087@iiita.ac.in>
2020-04-29 00:38:59 +0530

    Update models.py

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=100, default=None)
+    full_name = models.CharField(max_length=100, default=None)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(default=None)
+
+    def __str__(self):
+        return self.username
+
+class Granted_appointment(models.Model):
+    username = models.CharField(max_length=100, default=None)
+    full_name = models.CharField(max_length=100, default=None)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(default=None)
+
+    def __str__(self):
+        return self.username
+
