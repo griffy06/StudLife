bf4a2df4a93f5a3037a00f59ad387dde9ccb9964
Rohini Mishra <iit2018087@iiita.ac.in>
2020-04-29 02:43:13 +0530

    Update views.py

--- student/views.py
+++ student/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 from warden.models import Profile, Granted_outpasses
+from doctor.models import Profile, Granted_appointment
 from .render import Render
 
 
@@ -61,10 +62,6 @@ def outpass(request, user_id):
         return render(request, 'student/outpass_requested_page.html', {'user': user})
 
 
-def book_appointment(request, user_id):
-    return render(request, 'student/book_appointment_page.html')
-
-
 def order_food(request, user_id):
     return render(request, 'student/order_food_page.html')
 
@@ -97,3 +94,46 @@ def pdf(request,user_id):
     }
 
     return Render.render('student/pdf.html', params)
+def book_appointment(request, user_id):
+    if request.method == "GET":
+        user = User.objects.get(pk=user_id)
+        if user.student.appointments == 0:
+            return render(request, 'student/book_appointment_page.html', {'user': user})
+        elif user.student.appointments == 1:
+            return render(request, 'student/appointment_requested_page.html', {'user': user})
+        elif user.student.appointments == 2:
+            return render(request, 'student/appointment_booked_page.html', {'user': user})
+        elif user.student.appointments == 3:
+            return render(request, 'student/appointment_declined_page.html', {'user': user})
+
+    else:
+        user = User.objects.get(pk=user_id)
+        user.student.appointments = 1
+        user.save()
+        profile = Profile()
+        profile.username = user.username
+        profile.time = request.POST.get('time')
+        profile.date = request.POST.get('date')
+        profile.full_name = request.POST.get('full_name')
+        profile.save()
+        return render(request, 'student/book_appointment_page.html', {'user': user})
+
+
+def back_appointment(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.student.appointment = 0
+    user.save()
+    permit = Granted_appointment.objects.get(username=user.username)
+    permit.delete()
+    return redirect('book_appointment', user_id)
+
+def pdf_appointment(request,user_id):
+    user = User.objects.get(pk=user_id)
+    permit = Granted_appointment.objects.get(username=user.username)
+    go=permit.full_name
+    params = {
+        'full_name' : go,
+        'date' : permit.date,
+        'time': permit.time,
+    }
+    return Render.render('student/pdf_appointment.html', params)
