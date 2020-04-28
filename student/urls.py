8b43f2487b139d6b251381860bc8aad324dd798c
Rohini Mishra <iit2018087@iiita.ac.in>
2020-04-29 02:56:02 +0530

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<int:user_id>/book_appointment/',views.book_appointment, name="book_appointment"),
     path('<int:user_id>/digilocker/',views.digilocker, name="digilocker"),
     path('<int:user_id>/outpass/back',views.back_outpass, name="back_outpass"),
-    path('<int:user_id>/render/pdf',views.pdf, name="pdf")
+    path('<int:user_id>/render/pdf',views.pdf, name="pdf"),
     path('<int:user_id>/book_appointment/back',views.back_appointment, name="back_appointment"),
     path('<int:user_id>/render/pdf_appointment',views.pdf, name="pdf_appointment")
 
