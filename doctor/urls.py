19f41bb70904ea2be9b09b1d7dd94a12afb21515
Rohini Mishra <iit2018087@iiita.ac.in>
2020-04-29 21:06:25 +0530

    Update urls.py

--- doctor/urls.py
+++ doctor/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.UserFormView.as_view(), name="doctor_login"),
     path('view/', views.logged_in, name="doctor_logged_in"),
-    path('view/<int:profile_id>', views.individual_request, name="doctor_individual_request"),
+    path('view/<int:user_id>', views.show, name="show"),
+    path('viewlist/<int:profile_id>/', views.individual_request, name="doctor_individual_request"),
 ]
